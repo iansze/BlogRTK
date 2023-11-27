4d8da694bb027b3220d10a286ea78bdf06f1eb72
Ian Sze <iansze1124@gmail.com>
2023-11-27 17:25:22 -0500

    deploy

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+import cors from "cors";
+import path from "path";
+
+import express from "express";
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
+});
+
+app.use(express.static(path.join(path.resolve(), "./dist/index.html")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(path.resolve(), "./dist", "index.html"));
 });
