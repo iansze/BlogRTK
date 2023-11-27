ae52435eee098ce4b7b32e18b6c8d5d6beed89b2
Ian Sze <iansze1124@gmail.com>
2023-11-27 17:58:13 -0500

    deploy

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use("/api", jsonServerRouter);
 
-server.use(express.static(path.join(path.resolve(), "./dist/index.html")));
+server.use(express.static(path.join(path.resolve(), "dist")));
 server.get("*", (req, res) => {
-  res.sendFile(path.join(path.resolve(), "./dist", "index.html"));
+  res.sendFile(path.join(path.resolve(), "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
