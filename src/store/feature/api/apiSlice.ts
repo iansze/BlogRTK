a090cf9eb4cc4ad0ab4729d6c4ed0e1fda526138
Ian Sze <iansze1124@gmail.com>
2023-12-01 16:43:58 -0500

    update deploy

--- src/store/feature/api/apiSlice.ts
+++ src/store/feature/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:3000/api",
   }),
   //Defines the overall categories of tags that your endpoints will use
   tagTypes: ["Post", "id", "User"],
